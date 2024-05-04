1a1e7c8379070d554a192576f4fa49a074e13630
LTFan <xfqwdsj@qq.com>
2024-05-04 11:12:03 +0800

    chore: format code

--- client-py/src/nativeMain/kotlin/ByteArray.kt
+++ client-py/src/nativeMain/kotlin/ByteArray.kt
@@ -1,3 +1,21 @@
+/*
+ * This file is part of Fhraise.
+ * Copyright (c) 2024 HSAS Foodies. All Rights Reserved.
+ *
+ * Fhraise is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * Fhraise is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with Fhraise. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package xyz.xfqlittlefan.fhraise.py
 
 import kotlinx.cinterop.*
@@ -7,4 +25,6 @@ typealias OnData = CPointer<CFunction<(data: CArrayPointer<ByteVar>, size: Int) 
 
 @Deprecated("This function is for calling from C code only.", level = DeprecationLevel.HIDDEN)
 @ExperimentalForeignApi
-fun byteArrayToPointer(byteArray: ByteArray, onData: OnData) { memScoped { onData(allocArrayOf(byteArray), byteArray.size) } }
+fun byteArrayToPointer(byteArray: ByteArray, onData: OnData) {
+    memScoped { onData(allocArrayOf(byteArray), byteArray.size) }
+}
