24d0aec794241db13e667da69ad00f70c99f35c7
LTFan <xfqwdsj@qq.com>
2024-06-07 23:07:18 +0800

    chore: Kotlin 2.0 and Compose 1.6.10 (#64)

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ plugins {
     alias(libs.plugins.kotlinJvm) apply false
     alias(libs.plugins.kotlinSerialization) apply false
     alias(libs.plugins.jetbrainsCompose) apply false
+    alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
 }
